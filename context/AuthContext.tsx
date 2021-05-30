5a9356aac9410e5245873cdcaf9a584d4f3416ab
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-29 21:31:32 -0300

    first commit

---
+++ context/AuthContext.tsx
@@ -0,0 +1,110 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { api } from "../src/services/api";
+import Router from "next/router";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  isAuthenticated: boolean;
+  user: User;
+  signIn(credentials: SignInCredentials): Promise<void>;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export function signOut() {
+  destroyCookie(undefined, "nextauth.token");
+  destroyCookie(undefined, "nextauth.refreshToken");
+  Router.push("/");
+}
+
+const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState({} as User);
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      api
+        .get("me")
+        .then((response) => {
+          const { email, permissions, roles } = response?.data;
+
+          setUser({
+            email: email,
+            permissions,
+            roles,
+          });
+        })
+        .catch(() => {
+          signOut();
+        });
+    }
+  }, []);
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password,
+      });
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 dias
+        path: "/",
+      });
+
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 dias
+        path: "/",
+      });
+
+      setUser({
+        email: email,
+        permissions,
+        roles,
+      });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      Router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+
+  return context;
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import React, { FormEvent, useState } from "react";
+import { useAuthContext } from "../../context/AuthContext";
+import styles from "../../styles/Home.module.css";
+
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signIn } = useAuthContext();
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    await signIn(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className={styles.item}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className={styles.item}
+      />
+      <button type="submit" className={styles.item}>
+        Entrar
+      </button>
+    </form>
+  );
+}
